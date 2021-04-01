5f53a384f0ec070321fcb85e86e6f290f4d84bb1
Luke <luke.melas@gmail.com>
2021-04-01 00:26:49 -0400

    Added page title and meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,26 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
 
+const pageTitle = "Project Title"
+const description = "This is my description..."
+
+
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
         <Head />
+        {/* TODO: Set all these attributes */}
+        <title>{pageTitle}</title>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta property="og:site_name" content={pageTitle} key="ogsitename" />
+        {/* <meta property="og:url" content={TODO} key="ogurl" /> */}
+        {/* <meta name="twitter:card" content="{summary}" key="twcard" /> */}
+        {/* <meta name="twitter:creator" content={twitterHandle} key="twhandle" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
