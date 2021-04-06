3d2111e0b412fed49e1538d9ddcdedb536695ac3
Luke <luke.melas@gmail.com>
2021-04-06 15:19:51 -0400

    Update template

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Flex, FlexProps } from '@chakra-ui/react'
 
 export const Footer = (props: FlexProps) => (
-  <Flex as="footer" py="8rem" {...props} />
+  <Flex as="footer" py="4rem" {...props} />
 )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { Heading, Wrap, Link as ChakraLink } from '@chakra-ui/react'
+import { Heading, Wrap, Box, Link as ChakraLink } from '@chakra-ui/react'
 import { Container } from 'next/app'
 import NextLink from "next/link"
 
@@ -17,26 +17,26 @@ const authors = [
 
 export const Hero = ({ title }: { title: string }) => (
   <Container>
-    <Heading fontSize="3xl" pt="3rem">{title}</Heading>
+    <Heading fontSize="2xl" pt="3rem" maxW="42rem" textAlign="center">{title}</Heading>
     <Wrap justify="center" pt="1rem" fontSize="xl" key="authors">
       {
         authors.map((author) =>
-          <span key={author.name}>
+          <Box key={author.name} pl="1rem" pr="1rem">
             <NextLink href={author.url} passHref={true}>
               <ChakraLink>{author.name}</ChakraLink>
             </NextLink>
             <sup> {author.institutions.toString()}</sup>
-          </span>
+          </Box>
         )
       }
     </Wrap>
     <Wrap justify="center" pt="1rem" key="institutions">
       {
         Object.entries(institutions).map(tuple =>
-          <span>
+          <Box>
             <sup>{tuple[0]}  </sup>
             {tuple[1]}
-          </span>
+          </Box>
         )
       }
     </Wrap>
