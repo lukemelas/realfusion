d1a173ffdffbfb93f088a46470899dd5e6f8e024
Luke <luke.melas@gmail.com>
2023-02-06 15:35:41 +0000

    Updating info

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-#### Academic Website Project Template
+#### Website
 
 #### TODO:
 
 - [ ] Code
 - [ ] Gifs
 - [ ] Short video
+    - [x] Script
+    - [ ] Recording
+    - [ ] Editing
+- [ ] Longer video
+- [ ] Add website links to the paper
 
 Remember to edit `.env.production` with the correct path.
 
--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-    future: {
-        webpack5: true,
-    },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
     basePath: process.env.BASE_PATH,
-    assetPrefix: process.env.BASE_PATH + '/'
+    assetPrefix: process.env.BASE_PATH + '/'    
 }
+
+module.exports = nextConfig
